51965f634126a8776c47e1d72fe2790ebc5b5f50
justaguysk <in.case.of.melting@tutamail.com>
2026-08-31 13:43:07 +0200

    fixed faulty status code check

--- main.py
+++ main.py
@@ -152,11 +152,11 @@ def runLyricFinder():
         if r.status_code == 200: 
             rdict = json.loads(r.text)
             lyrics = rdict["lyrics"].replace('\n', '<br>')
-            
+
         elif ('-' in currentArtist) or ('/' in currentArtist) or ('-' in currentTitle) or ('/' in currentTitle):
             r2 = requests.get(f'{ENDPOINT}/{quote(currentArtist)}/{quote(currentTitle)}')
 
-            if r.status_code == 200: 
+            if r2.status_code == 200: 
                 rdict = json.loads(r2.text)
                 lyrics = rdict["lyrics"].replace('\n', '<br>')
             
